5346fded4a7f3e036440cb45a5ad79145cb1343a
mkht <mkht@users.noreply.github.com>
2017-12-30 19:22:20 +0900

    Fix #1 Remove unnecessary code

--- PSWebDriver.psm1
+++ PSWebDriver.psm1
@@ -1,21 +1,7 @@
-﻿# Load Classes
+# Load Classes
 $ClassList = @(
     'PSWebDriver.ps1'
 )
 foreach ($class in $ClassList) {
     . $PSScriptRoot\Class\$class
 }
-
-# Load Functions
-$AllFunctions = @( Get-ChildItem -Path $PSScriptRoot\Function\ -Filter *.ps1 -Recurse -File -ErrorAction SilentlyContinue )
-foreach ($function in $AllFunctions) {
-    try {
-        # Dot source
-        . $function.fullname
-    }
-    catch {
-        Write-Error -Message "Failed to import function $($function.fullname): $_"
-    }
-}
-
-Export-ModuleMember -Function $AllFunctions.Basename
