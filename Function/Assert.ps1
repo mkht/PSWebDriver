95d7b212d5c9e943799fc96426852e4e4f4cc7fc
mkht <mkht@users.noreply.github.com>
2018-01-15 00:26:23 +0900

    Refactoring Assert function

--- Function/Assert.ps1
+++ Function/Assert.ps1
@@ -22,7 +22,8 @@
         $Not
     )
 
-    $code = @'
+    Begin {
+        $code = @'
     using System;
     public class AssertionFailedException : Exception
     {
@@ -32,48 +33,51 @@
     }
 '@
 
-    Add-Type -Language CSharp -TypeDefinition $code -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-
-    $Result = $false
-    $Message
-
-    if ($Matcher -eq 'Equal') {
-        $Result = [bool]($AssertValue -eq $Expected)
-        if ($Not) {
-            $Result = !$Result
-            $Message = ("Expected: value was [{0}], but should not have been the same" -f [string]$Expected)
-        }
-        else {
-            $Message = ("Expected [{0}] but was [{1}]" -f [string]$Expected, [string]$AssertValue)
-        }
-    }
-    elseif ($Matcher -eq 'RegExp') {
-        $Result = [bool]($AssertValue -match $Expected)
-        if ($Not) {
-            $Result = !$Result
-            $Message = ("Expected: [{0}] to not match the expression [{1}]" -f [string]$AssertValue, [string]$Expected)
-        }
-        else {
-            $Message = ("Expected: [{0}] to match the expression [{1}]" -f [string]$AssertValue, [string]$Expected)
-        }
-    }
-    elseif ($Matcher -eq 'Like') {
-        $Result = [bool]($AssertValue -like $Expected)
-        if ($Not) {
-            $Result = !$Result
-            $Message = ("Expected: [{0}] not to be like the wildcard [{1}]" -f [string]$AssertValue, [string]$Expected)
-        }
-        else {
-            $Message = ("Expected: [{0}] to be like the wildcard [{1}]" -f [string]$AssertValue, [string]$Expected)
-        }
+        Add-Type -Language CSharp -TypeDefinition $code -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
     }
 
-    if ($CustomMessage) {
-        $Message = $CustomMessage
-    }
+    Process {
+        $Result = $false
+        $Message
 
-    if (!$Result) {
-        throw (New-Object AssertionFailedException $Message)
+        if ($Matcher -eq 'Equal') {
+            $Result = [bool]($AssertValue -eq $Expected)
+            if ($Not) {
+                $Result = !$Result
+                $Message = ("Expected: value was [{0}], but should not have been the same" -f [string]$Expected)
+            }
+            else {
+                $Message = ("Expected [{0}] but was [{1}]" -f [string]$Expected, [string]$AssertValue)
+            }
+        }
+        elseif ($Matcher -eq 'RegExp') {
+            $Result = [bool]($AssertValue -match $Expected)
+            if ($Not) {
+                $Result = !$Result
+                $Message = ("Expected: [{0}] to not match the expression [{1}]" -f [string]$AssertValue, [string]$Expected)
+            }
+            else {
+                $Message = ("Expected: [{0}] to match the expression [{1}]" -f [string]$AssertValue, [string]$Expected)
+            }
+        }
+        elseif ($Matcher -eq 'Like') {
+            $Result = [bool]($AssertValue -like $Expected)
+            if ($Not) {
+                $Result = !$Result
+                $Message = ("Expected: [{0}] not to be like the wildcard [{1}]" -f [string]$AssertValue, [string]$Expected)
+            }
+            else {
+                $Message = ("Expected: [{0}] to be like the wildcard [{1}]" -f [string]$AssertValue, [string]$Expected)
+            }
+        }
+
+        if ($CustomMessage) {
+            $Message = $CustomMessage
+        }
+
+        if (!$Result) {
+            throw (New-Object AssertionFailedException $Message)
+        }
     }
 
 }
