36dbb05c22018a374aff2ce5291f7c38e642e705
mkht <mkht@users.noreply.github.com>
2020-02-18 23:28:17 +0900

    Revert "fail"
    This reverts commit c57a58314431c595ffe8dcd21dd8884385228aac.

--- Tests/PSWebDriver/PSWebDriver.tests.ps1
+++ Tests/PSWebDriver/PSWebDriver.tests.ps1
@@ -522,10 +522,6 @@ InModuleScope PSWebDriver {
                     try { $Driver.Start($TestURL) }catch { } # Whether an exception raises depend on the browser (Firefox will throw, but Chrome will not.)
                     $Driver.IsElementPresent('id=content') | Should -BeFalse
                 }
-
-                It 'FAIL' {
-                    $false | Should -BeTrue
-                }
             }
         }
     }
